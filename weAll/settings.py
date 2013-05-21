ebb4f709fdfaead2e93ee5a0973fb46c119f3337
Arash Khajelou <a.khajelou@gmail.com>
2013-05-22 00:12:27 +0430

    first setting for starting first login page

--- weAll/settings.py
+++ weAll/settings.py
@@ -4,19 +4,19 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Arash', 'a.khajelou@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'we_all_db',                           # Or path to database file if using sqlite3.
+        'USER': 'root',                           # Not used with sqlite3.
+        'PASSWORD': 'mysql123',                       # Not used with sqlite3.
+        'HOST': 'localhost',                           # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',                           # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -117,9 +117,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
     'login',
 )
 
